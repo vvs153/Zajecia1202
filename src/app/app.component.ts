f2db4deb9f3136cb08b6f9e3ffe71f3d8b57508e
Kuba Postol <vvs153@gmail.com>
2023-03-17 16:25:21 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Zajecia1202';
+  ADRES_BAZOWY ="http://api.weatherapi.com/v1"
+  ENDPOINT_PROGNOZA = "/forecast.json"
+  ENDPOINT_OBECNA = "/current.json"
+  ENDPOINT_MECZE = "/sports.json"
+  PARAMETR_KLUCZ = "key"
+  PARAMETR_DNI = "days"
+  PARAMETR_LOKALIZACJA = "q"
+  constructor(private http: HttpClient) {
+    this.sprawdzPogode("224add26cf4c4e7c8e8141024231202",2,"Oslo" )
+  }
+
+  sprawdzPogode(kluczAPI: string, dni: number, lokalizacja: string) {
+    this.http.get(this.ADRES_BAZOWY + this.ENDPOINT_PROGNOZA, {
+      params: {
+        key: kluczAPI,
+        days: dni,
+        q: lokalizacja
+      }
+    }).subscribe({
+      next: value => {
+        console.log(value)
+      },
+      error: err => {
+        console.log("Error")
+        console.log(err)
+      }
+    })
+  }
 }
